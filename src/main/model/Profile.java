e5464cfdb206c2b8f266cc41d16f0359305a4bcb
krismah <kristofermah@gmail.com>
2022-10-10 01:57:21 -0700

    implemented methods in the Profile class

--- src/main/model/Profile.java
+++ src/main/model/Profile.java
@@ -2,42 +2,50 @@ package model;
 
 // Represents a profile with a name, faculty, commute route and short message
 public class Profile {
+    private String name;
+    private String faculty;
+    private int route;
+    private String message;
 
     // EFFECTS: creates a profile with a name, faculty, commute route and short message
     public Profile(String name, String faculty, int route, String message) {
+        this.name = name;
+        this.faculty = faculty;
+        this.route = route;
+        this.message = message;
 
     }
 
     public String getName() {
-        return ""; //stub
+        return name;
     }
 
     public String getFaculty() {
-        return ""; //stub
+        return faculty;
     }
 
     public int getRoute() {
-        return 0; //stub
+        return route;
     }
 
     public String getMessage() {
-        return ""; //stub
+        return message;
     }
 
-    public void setName() {
-        // stub
+    public void setName(String newName) {
+       name = newName;
     }
 
-    public void setFaculty() {
-        // stub
+    public void setFaculty(String newFaculty) {
+        faculty = newFaculty;
     }
 
-    public void setRoute() {
-        // stub
+    public void setRoute(int newRoute) {
+        route = newRoute;
     }
 
-    public void setMessage() {
-        // stub
+    public void setMessage(String newMessage) {
+        message = newMessage;
     }
 
 }
