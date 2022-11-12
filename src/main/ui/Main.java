02f6d7fe86fd08719d931a5181d469256eed8953
krismah <kristofermah@gmail.com>
2022-11-12 00:14:34 -0800

    started building the GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,13 +1,15 @@
 package ui;
 
+import ui.gui.HomePageGUI;
+
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            new SocialApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found, unable to run application.");
-        }
+//        try {
+            new HomePageGUI();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("File not found, unable to run application.");
+//        }
     }
 }
---
+++ src/main/ui/gui/HomePageGUI.java
@@ -0,0 +1,19 @@
+package ui.gui;
+
+import javax.swing.*;
+
+public class HomePageGUI extends JFrame {
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 500;
+
+    public HomePageGUI() {
+        super("Find Your Community!");
+        setSize(WIDTH, HEIGHT);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+
+
+    }
+}
