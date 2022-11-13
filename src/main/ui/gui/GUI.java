2a9550e39f90c18d918a9a4ea25ef411ca8cb473
krismah <kristofermah@gmail.com>
2022-11-12 21:02:51 -0800

    have functionality related to adding multiple Xs to Y

--- src/main/ui/gui/GUI.java
+++ src/main/ui/gui/GUI.java
@@ -7,7 +7,6 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -15,7 +14,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-import java.util.Scanner;
 
 import static javax.swing.ListSelectionModel.SINGLE_SELECTION;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
@@ -30,12 +28,14 @@ public class GUI {
     private static final String JSON_STORE = "./data/account.json";
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
-    private static final int HEIGHT = 500;
-    private static final int WIDTH = 500;
+    private static final int HEIGHT = 5000;
+    private static final int WIDTH = 5000;
     private JFrame frame;
     private JPanel saveAndLoad;
     private JList graphicalDatabase;
     private JPanel userGraphicalDatabase;
+    private JList graphicalUserList;
+    private JPanel userGraphicalUserList;
 
     public GUI() {
         frame = new JFrame("Find Your Community!");
@@ -46,9 +46,11 @@ public class GUI {
         initialize();
         initializeSaveAndLoadButtons();
         initializeDatabase();
+        initializeUserList();
 
         frame.add(saveAndLoad, BorderLayout.SOUTH);
-        frame.add(userGraphicalDatabase);
+        frame.add(userGraphicalDatabase, BorderLayout.WEST);
+        frame.add(userGraphicalUserList, BorderLayout.EAST);
         frame.pack();
         frame.setVisible(true);
 
@@ -59,18 +61,21 @@ public class GUI {
         Profile sampleUser1 = new Profile("Tim", "Land and Food Systems", 99, "Hello!");
         Profile sampleUser2 = new Profile("Karen", "Science", 33, "I love bacteria!");
         Profile sampleUser3 = new Profile("Nathan", "Arts", 49, "Howdy!");
+        Profile sampleUser4 = new Profile("Marie", "Science", 99, "Chemistry-fanatic!");
+        Profile sampleUser5 = new Profile("Tyler", "Sauder School of Business", 49, "Money money money!");
         acc = new Account();
         database = acc.getDatabase();
         database.addProfile(sampleUser1);
         database.addProfile(sampleUser2);
         database.addProfile(sampleUser3);
+        database.addProfile(sampleUser4);
+        database.addProfile(sampleUser5);
         userList = acc.getUserList();
     }
 
     // EFFECTS: initializes the saving and loading buttons and features
     private void initializeSaveAndLoadButtons() {
         saveAndLoad = new JPanel(new GridLayout(2, 1));
-        saveAndLoad.setBorder(BorderFactory.createLineBorder(Color.black));
         JButton saveButton = new JButton("Save the Application");
 
         saveButton.addActionListener(new ActionListener() {
@@ -97,8 +102,8 @@ public class GUI {
         DefaultListModel listModel = new DefaultListModel();
 
         for (Profile profile : acc.getDatabase().getList()) {
-            listModel.addElement(profile.getName() + " " + profile.getFaculty()
-                    + "\n" + profile.getRoute() + "\n" + profile.getMessage());
+            listModel.addElement(profile.getName() + " || " + profile.getFaculty()
+                    + " || " + profile.getRoute() + " || " + profile.getMessage());
         }
 
         graphicalDatabase = new JList(listModel);
@@ -115,22 +120,22 @@ public class GUI {
             }
         });
         graphicalDatabase.setSelectionMode(SINGLE_SELECTION);
-        graphicalDatabase.setVisibleRowCount(5);
-
+        graphicalDatabase.setSelectedIndex(0);
+        graphicalDatabase.setVisibleRowCount(3);
         createDatabaseButtons();
     }
 
     private void createDatabaseButtons() {
-        JLabel databaseLabel = new JLabel("Profile Database");
-        JButton addProfile = new JButton("Add Profile");
+        JLabel databaseLabel = new JLabel("Profile Database" + " (Name, Faculty, Route, and Message)");
+        JButton addProfile = new JButton("Add Profile to Your List");
         addProfile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("slay");
+                addProfileAction();
             }
         });
 
-        JButton sortProfiles = new JButton("Sort Profiles");
+        JButton sortProfiles = new JButton("Sort Database by Route");
         sortProfiles.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -146,6 +151,84 @@ public class GUI {
         userGraphicalDatabase.add(databaseLabel, BorderLayout.NORTH);
         userGraphicalDatabase.add(graphicalDatabase, BorderLayout.CENTER);
         userGraphicalDatabase.add(buttonPanel, BorderLayout.SOUTH);
+    }
 
+    // EFFECTS: initializes the graphical representation of userlist
+    private void initializeUserList() {
+        DefaultListModel listModel = new DefaultListModel();
+
+        for (Profile profile : acc.getUserList().getList()) {
+            listModel.addElement(profile.getName() + " || " + profile.getFaculty()
+                    + " || " + profile.getRoute() + " || " + profile.getMessage());
+        }
+        graphicalUserList = new JList(listModel);
+        graphicalUserList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (e.getValueIsAdjusting() == false) {
+                    if (graphicalUserList.getSelectedIndex() == -1) {
+                        System.out.println("case 1");
+                    } else {
+                        System.out.println("case 2");
+                    }
+                }
+            }
+        });
+        graphicalUserList.setSelectionMode(SINGLE_SELECTION);
+        graphicalUserList.setSelectedIndex(0);
+        graphicalUserList.setVisibleRowCount(3);
+        createUserListButtons();
+    }
+
+    private void createUserListButtons() {
+        JLabel userListLabel = new JLabel("Your List of Profiles");
+        JButton removeProfile = new JButton("Remove Profile");
+        removeProfile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeProfileAction();
+            }
+        });
+
+        JButton sortProfiles = new JButton("Sort List by Route");
+        sortProfiles.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("sort profiles button");
+            }
+        });
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(removeProfile);
+        buttonPanel.add(sortProfiles);
+
+        userGraphicalUserList = new JPanel(new BorderLayout());
+        userGraphicalUserList.add(userListLabel, BorderLayout.NORTH);
+        userGraphicalUserList.add(graphicalUserList, BorderLayout.CENTER);
+        userGraphicalUserList.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void addProfileAction() {
+        userList.addProfile(database.getList().get(graphicalDatabase.getSelectedIndex()));
+        DefaultListModel list = new DefaultListModel();
+
+        for (Profile profile : acc.getUserList().getList()) {
+            list.addElement(profile.getName() + " || " + profile.getFaculty()
+                    + " || " + profile.getRoute() + " || " + profile.getMessage());
+        }
+
+        graphicalUserList.setModel(list);
+    }
+
+    private void removeProfileAction() {
+        userList.removeProfile(userList.getList().get(graphicalUserList.getSelectedIndex()));
+        DefaultListModel list = new DefaultListModel();
+
+        for (Profile profile : acc.getUserList().getList()) {
+            list.addElement(profile.getName() + " || " + profile.getFaculty()
+                    + " || " + profile.getRoute() + " || " + profile.getMessage());
+        }
+
+        graphicalUserList.setModel(list);
     }
 }
